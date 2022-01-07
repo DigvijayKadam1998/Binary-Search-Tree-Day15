e88c4fd20ce3c76759ddaf089a35c245eb16436e
[Digvikadam] <[digvikadam168@gmail.com]>
2022-01-07 17:16:25 +0530

    Welcome to BST Problem

---
+++ src/com/bridgelabz/binarysearchtree/TestBinarySearchTree.java
@@ -0,0 +1,18 @@
+package com.bridgelabz.binarysearchtree;
+
+public class TestBinarySearchTree {
+	public static void main(String[] args) {
+		BinarySearchTree bst = new BinarySearchTree();
+		
+		Node root = bst.insertElement(null, 56);
+		Node node = bst.insertElement(root, 30);
+		Node nextNode = bst.insertElement(root, 70);
+		
+		System.out.println("InOrder Traversal: ");
+		bst.inOrder(root);
+		System.out.println("\n\nPreOrder Traversal: ");
+		bst.preOrder(root);
+		System.out.println("\n\nPostOrder Traversal: ");
+		bst.postOrder(root);
+	}
+}
